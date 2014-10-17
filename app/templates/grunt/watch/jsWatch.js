e59feb555614ff39eddc6a5cbd17872e1228b366
Lev Brie <lev.brie@gmail.com>
2014-10-16 23:32:16 -0700

    CHORE(jshint): migrate mean-scaffold changes for jshintrc to templates

--- app/templates/grunt/watch/jsWatch.js
+++ app/templates/grunt/watch/jsWatch.js
@@ -1,10 +1,10 @@
 var projectFiles = require('../files.json');
 // console.log(projectFiles);
-console.log(projectFiles['js']);
+console.log(projectFiles.js);
 console.log('public');
 // console.log(projectFiles['js']['public']);
 module.exports = {
   files: [projectFiles.js.public, projectFiles.js.server],
-  // files: ["<%%= files.js.public %>", "<%%= files.js.server %>"],
+  // files: ["<%= files.js.public %>", "<%= files.js.server %>"],
   tasks: ['reload', 'uglify']
 };
